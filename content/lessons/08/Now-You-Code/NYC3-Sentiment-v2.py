5f711df635ba88e74ca4abfdb65fb9c50cbc5d88
Michael Fudge <mafudge@gmail.com>
2016-12-30 09:43:02 -0500

    lesson 08

---
+++ content/lessons/08/Now-You-Code/NYC3-Sentiment-v2.py
@@ -0,0 +1,27 @@
+'''
+Now You Code 3: Sentiment 2.0
+
+Let's improve on our basic sentiment analyzer in Python.
+
+Copy the code from 1.0 most of it will be the same.
+
+We will improve on this program by writing a user-defined
+function load_words() to load the positve or negative words from a file.
+
+For example, instead of:
+
+pos = "happy glad joy"
+
+we do this:
+
+pos = load_words("NYC3-pos.txt")
+
+We can now make the program "smarter" simply by adding positive and
+negative words to the text files!
+
+
+'''
+
+# TODO: Write Todo list then beneath write your code
+
+# Write code here
---
+++ content/lessons/08/Now-You-Code/NYC4-Email-Harvest-Training.py
@@ -0,0 +1,31 @@
+'''
+Now You Code 4: Email Harvesting Training
+
+Description
+
+Write a program to extract the email addresses from the mailbox
+file "NYC4-mbox-short.txt" and save them into another file
+"NYC4-emails.txt" with one extracted email address per line.
+
+The best way to find the emails in the mailbox file is to search
+for lines in the file that begin with "From:". When you find an email
+write just the address (not "From:" and the address) to
+"NYC4-emails.txt", and don't worry about duplicates.
+
+The program should print the number of emails it wrote to the file.
+
+Example Run:
+
+Wrote 27 emails to NYC4-emails.txt
+
+
+After 100000 flips. Heads: 50030, Tails 49970
+
+Start out your program by writing your TODO list of steps
+you'll need to solve the problem!
+'''
+
+# TODO: Write Todo list then beneath write your code
+
+# Write code here 
+
